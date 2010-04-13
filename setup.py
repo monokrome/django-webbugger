5d929c4132ff1b1ca7f22e0087827e54f0617610
Brandon R. Stoner <monokrome@monokro.me>
2010-04-13 06:23:07 -0700

    Thought that someone might want to use distutils to install this.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='django-webbugger',
+      version='0.1',
+      description='Basic webbugger/tracking beacon for django',
+      author='Brandon R. Stoner',
+      author_email='monokrome@monokro.me',
+      url='http://monokro.me/',  # This needs updated once my site is active.
+      packages=['django_webbugger'],
+     )
+
+
