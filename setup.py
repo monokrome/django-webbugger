bf07f6a30521a2341c856736284c606f4322dee7
Brandon R. Stoner <github@monokro.me>
2010-10-15 16:43:23 -0600

    Moved webbugger out of the django.contrib namespace.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='django-webbugger',
       author='Brandon R. Stoner',
       author_email='monokrome@monokro.me',
       url='http://monokro.me/',  # This needs updated once my site is active.
-      packages=['django.contrib.webbugger'],
+      packages=['webbugger'],
      )
 
 
